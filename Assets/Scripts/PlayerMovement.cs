4bfa48078b2e47d74f180b13cc9fa03da28c0334
David-T10 <54846690+David-T10@users.noreply.github.com>
2024-02-12 17:27:25 +0000

    player movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       Debug.Log("Hi");
+        
     }
 
     // Update is called once per frame
