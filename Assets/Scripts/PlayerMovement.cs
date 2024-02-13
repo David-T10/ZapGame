ecd95e7c50576e093b4bb70ca09a91a03b372918
David-T10 <54846690+David-T10@users.noreply.github.com>
2024-02-13 00:16:45 +0000

    added a player movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Vector2 initialVelocity = Vector2.zero;
+    private Rigidbody2D rb;
+    private void Awake()
     {
-        
+      rb = gameObject.GetComponent<Rigidbody2D>();  
+
+    }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb.velocity = initialVelocity;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x,10f);
+        }
+
+        float xVal = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(xVal * 7f, rb.velocity.y);
     }
 }
