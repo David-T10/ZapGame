ae734e700a45e1801f064463d7cb991a2a8cca89
David-T10 <54846690+David-T10@users.noreply.github.com>
2024-02-14 20:50:30 +0000

    Added banana items
    Using a dictionary and datastruct for fruit items, neeed to add power up methods to both

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -5,21 +5,71 @@ using UnityEngine.UI;
 
 public class ItemPickup : MonoBehaviour
 {
+    public struct FruitData
+    {
+        public Color color;
+        public int count;
+
+        public FruitData(Color color, int count)
+        {
+            this.color = color;
+            this.count = count;
+        }
+    }
+
+    private Dictionary<string, FruitData> fruitDataMap = new Dictionary<string, FruitData>();
+
     private SpriteRenderer playerSprite;
-    private int apples = 0;
     Color orange = new Color(1f, 0.5f, 0f, 1f);
-    [SerializeField] private Text ItemCounterText;
+    Color yellow = new Color(1f, 0.92f, 0.016f, 1f);
+    private Color originalSpriteColor;
+    private float powerUpDuration = 10.0f;
+    private int startingFruitsCount = 0;
+    [SerializeField] private Text appleCounterText;
+    [SerializeField] private Text bananaCounterText;
 
     private void Awake() {
+
         playerSprite = GetComponent<SpriteRenderer>();
+        originalSpriteColor = playerSprite.color;
+        fruitDataMap.Add("Apple", new FruitData(orange, startingFruitsCount));
+        fruitDataMap.Add("Banana", new FruitData(yellow, startingFruitsCount));
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.gameObject.CompareTag("Apple")) {
+        if (fruitDataMap.ContainsKey(collision.gameObject.tag))
+        {
+            
+            FruitData fruitData = fruitDataMap[collision.gameObject.tag];
             Destroy(collision.gameObject);
-            apples++;
-            ItemCounterText.text = "Apples: " + apples;
-            playerSprite.color = orange;
+            fruitData.count++;
+            playerSprite.color = fruitData.color;
+            StartCoroutine(RevertColorAfter(powerUpDuration));
+
+            if (collision.gameObject.tag == "Apple")
+            {
+                appleCounterText.text = "Apples: " + fruitData.count;
+                ApplePowerUp();
+            }
+            else if (collision.gameObject.tag == "Banana")
+            {
+                bananaCounterText.text = "Bananas: " + fruitData.count;
+                BananaPowerUp();
+            }
         }
+        }
+
+    private void ApplePowerUp() {
+    }
+
+    private void BananaPowerUp()
+    {
+    }
+
+
+    private IEnumerator RevertColorAfter(float timeDelay) {
+        yield return new WaitForSeconds(timeDelay);
+        playerSprite.color = originalSpriteColor;
     }
 }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator animator;
+    private Rigidbody2D rb2d;
+
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("InstantKillTrap")) {
+            Death();
+        } 
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void Death() {
+        rb2d.bodyType = RigidbodyType2D.Static;
+        animator.SetTrigger("death");
     }
 }
